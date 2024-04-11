905b193e4967fef60da905b2bdbd40aa0f0e1616
dlwodn1414 <dlwodn1414@mindbloom.co.kr>
2024-04-11 18:07:57 +0900

    반응형 공부하기

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,25 @@
 <template>
-	<div></div>
+	<div>
+		<p>author: {{ author }}</p>
+		<p>title: {{ title }}</p>
+	</div>
 </template>
 
 <script>
+import { reactive } from "vue";
 export default {
 	setup() {
-		return {};
+		const book = reactive({
+			author: "Vue Team",
+			year: "2020",
+			title: "Vue 3 Giude",
+			descriprtion: "당신은 읽습니다.",
+			price: "무료",
+		});
+
+		const { author, title } = book;
+		console.log(typeof author);
+		return { author, title, book };
 	},
 };
 </script>
---
+++ src/lectures/7/App_ref.vue
@@ -0,0 +1,29 @@
+<template>
+	<div>
+		<p>{{ message }}</p>
+		<!-- 반응형 동작 확인 -->
+		<button v-on:click="addMessage">Click!</button>
+	</div>
+</template>
+
+<script>
+import { reactive, ref } from "vue";
+export default {
+	setup() {
+		// const는 상수기 때문에 let으로 변경
+		// 속성을 사용했기에 .value로 연결해서 사용해야 한다.
+		let message = ref("Hello Vue!");
+		const addMessage = () => {
+			message.value = message.value + "!";
+		};
+		console.log("message", message.value);
+		console.log("message", typeof message.value);
+		return {
+			message,
+			addMessage,
+		};
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
