cc11607e2449c98988b2f01af2693d18717493ce
dlwodn1414 <dlwodn1414@mindbloom.co.kr>
2024-07-26 11:27:09 +0900

    소스추가

---
+++ src/components/LifeCycleChild.vue
@@ -0,0 +1,32 @@
+<template>
+	<div class="card">
+		<div class="card-body">Lifecycle Child</div>
+		<input id="input" type="text" />
+	</div>
+</template>
+
+<script>
+import { onBeforeMount, onBeforeUnmount, onMounted, onUnmounted } from "vue";
+export default {
+	setup() {
+		//console.log("[child] setup");
+		onBeforeMount(() => {
+			console.log("[child] onBeforeMount");
+		});
+		onMounted(() => {
+			console.log("[child] onMount");
+		});
+		onBeforeUnmount(() => {
+			console.log("[Child] onBeforeUnmount");
+			console.log(document.querySelector("#input"));
+		});
+		onUnmounted(() => {
+			console.log("[Child] onUnmounted");
+			console.log(document.querySelector("#input"));
+		});
+		return {};
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
--- src/components/LifeCycleHooks.vue
+++ src/components/LifeCycleHooks.vue
@@ -1,24 +1,49 @@
 <template>
 	<div class="container py-4">
-		<input ref="inputRef" type="text" />
+		<button @click="visible = !visible">Toggle Child</button>
+		<LifeCycleChild v-if="visible"></LifeCycleChild>
+		<p id="message">{{ message }}</p>
 	</div>
 </template>
 
 <script>
-import { onBeforeMount, onMounted, ref } from "vue";
+import { onBeforeMount, onBeforeUpdate, onMounted, onUpdated, ref } from "vue";
+import LifeCycleChild from "./LifeCycleChild.vue";
 
 export default {
+	components: {
+		LifeCycleChild,
+	},
 	setup() {
-		console.log("setup");
+		// console.log("setup");
 		const inputRef = ref(null);
-		onBeforeMount(() => {
-			console.log("onBeforeMount");
-		});
-		onMounted(() => {
-			console.log("onMounted", inputRef.value);
-		});
+		const message = ref("");
+		const visible = ref(false);
+		//onBeforeMount는 mount되기 전이라 출력X
+		// onBeforeMount(() => {
+		// 	console.log("onBeforeMount", inputRef.value);
+		// });
+		//onMounted는 mount된 이후이기 때문에 출력
+		// onMounted(() => {
+		// 	console.log("onMounted", inputRef.value.value);
+		// });
 
-		return { inputRef };
+		// onBeforeUpdate(() => {
+		// 	console.log("onBefroeUpdate", message.value);
+		// 	console.log(
+		// 		"DOM Content:",
+		// 		document.querySelector("#message").textContent,
+		// 	);
+		// });
+		// onUpdated(() => {
+		// 	console.log("onUpdated", message.value);
+		// 	console.log(
+		// 		"DOM Content:",
+		// 		document.querySelector("#message").textContent,
+		// 	);
+		// });
+
+		return { inputRef, message, visible };
 	},
 	// beforeCreate() {
 	// 	console.log("beforeCreate");
