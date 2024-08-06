28af7465f07e80c86c30947f3169d4861687fe55
dlwodn1414 <dlwodn1414@mindbloom.co.kr>
2024-08-06 14:57:40 +0900

    Vue3 기본편 끝

---
+++ src/components/DynamicComponent.vue
@@ -0,0 +1,32 @@
+<template>
+	<div class="container py-4">
+		<button
+			class="btn btn-primary me-2"
+			@click="changeCurrentComp(DynamicApple)"
+		>
+			사과
+		</button>
+		<button class="btn btn-danger" @click="changeCurrentComp(DynamicBanana)">
+			바나나
+		</button>
+		<hr />
+		<component :is="currentComp" />
+		<p>{{ obj1 }}</p>
+		<p>{{ obj2 }}</p>
+	</div>
+</template>
+
+<script setup>
+import { ref, shallowRef } from "vue";
+import DynamicApple from "./DynamicApple.vue";
+import DynamicBanana from "./DynamicBanana.vue";
+
+const currentComp = shallowRef(DynamicApple); //ref보다 shallowRef를 사용하는 것이 좋다
+const obj1 = ref({ name: "홍길동" });
+
+// shallowRef는 속성이 아닌 값 자체가 변경 되었을 때만 변경됨.
+const obj2 = shallowRef({ name: "김길동" });
+const changeCurrentComp = comp => (currentComp.value = comp);
+</script>
+
+<style lang="scss" scoped></style>
