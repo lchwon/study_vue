ac322da455fd279e1340e0e59983c7de48f2e5d5
dlwodn1414 <dlwodn1414@mindbloom.co.kr>
2024-10-14 17:39:36 +0900

    수정

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
 	<div>
-		<TheNav></TheNav>
-		<TheView></TheView>
+		<!-- <TheNav></TheNav>
+		<TheView></TheView> -->
+		<test1></test1>
 	</div>
 </template>
 
 <script>
-import TheNav from "./components/TheNav.vue";
-import TheView from "./components/TheView.vue";
+// import TheNav from "./components/TheNav.vue";
+// import TheView from "./components/TheView.vue";
+import test1 from "./lectures/1-test/test.vue";
 export default {
 	components: {
-		TheNav,
-		TheView,
+		// TheNav,
+		// TheView,
+		test1,
 	},
 	setup() {
 		return {};
---
+++ src/lectures/1-test/test.vue
@@ -0,0 +1,38 @@
+<template>
+	<div>
+		<div :class="{ active: weActive, 'text-dager': hasError }">
+			텍스트 입니다.
+		</div>
+		<button v-on:click="toggle">toggle</button>
+		<button>toggleError</button>
+	</div>
+</template>
+
+<script>
+import { ref } from "vue";
+
+export default {
+	setup() {
+		const weActive = ref(true);
+		const hasError = ref(false);
+
+		const toggle = () => {
+			weActive.value = !weActive.value;
+		};
+
+		return { weActive, hasError, toggle };
+	},
+};
+</script>
+
+<style scoped>
+.active {
+	font-size: 30px;
+	color: rebeccapurple;
+}
+
+.text-dager {
+	color: aqua;
+	font-weight: 700;
+}
+</style>
