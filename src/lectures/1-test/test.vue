20232ee2ba6ceca9d001fbbad090ec0aa7bb6b8a
dlwodn1414 <dlwodn1414@mindbloom.co.kr>
2024-10-15 18:01:08 +0900

    복기

--- src/lectures/1-test/test.vue
+++ src/lectures/1-test/test.vue
@@ -1,38 +1,13 @@
 <template>
-	<div>
-		<div :class="{ active: weActive, 'text-dager': hasError }">
-			텍스트 입니다.
-		</div>
-		<button v-on:click="toggle">toggle</button>
-		<button>toggleError</button>
-	</div>
+	<div></div>
 </template>
 
 <script>
-import { ref } from "vue";
-
 export default {
 	setup() {
-		const weActive = ref(true);
-		const hasError = ref(false);
-
-		const toggle = () => {
-			weActive.value = !weActive.value;
-		};
-
-		return { weActive, hasError, toggle };
+		return {};
 	},
 };
 </script>
 
-<style scoped>
-.active {
-	font-size: 30px;
-	color: rebeccapurple;
-}
-
-.text-dager {
-	color: aqua;
-	font-weight: 700;
-}
-</style>
+<style lang="scss" scoped></style>
