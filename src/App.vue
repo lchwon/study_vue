cc6135856483bd3842c598bfc98869451b2be4bf
dlwodn1414 <dlwodn1414@mindbloom.co.kr>
2024-10-17 17:46:38 +0900

    수정

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,21 @@
 <template>
 	<div>
-		<!-- <TheNav></TheNav>
-		<TheView></TheView> -->
-		<test1></test1>
+		<TheNav></TheNav>
+		<TheView></TheView>
+		<!-- <test1></test1> -->
 	</div>
 </template>
 
 <script>
-// import TheNav from "./components/TheNav.vue";
-// import TheView from "./components/TheView.vue";
+import TheNav from "./components/TheNav.vue";
+import TheView from "./components/TheView.vue";
 //import test1 from "./lectures/1-test/test.vue";
-import test1 from "./lectures/1-test/TheView.vue";
+//import test1 from "./lectures/1-test/TheView.vue";
 export default {
 	components: {
-		// TheNav,
-		test1,
+		TheNav,
+		TheView,
+		//test1,
 	},
 	setup() {
 		return {};
